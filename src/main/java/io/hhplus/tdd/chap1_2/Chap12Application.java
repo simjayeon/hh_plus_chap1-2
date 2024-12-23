6e65df6bdca4f068a8b2369a17614ab103cc3e68
nature <nature@letitu.io>
2024-12-23 14:20:54 +0900

    project settings, mysql connection

---
+++ src/main/java/io/hhplus/tdd/chap1_2/Chap12Application.java
@@ -0,0 +1,13 @@
+package io.hhplus.tdd.chap1_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap12Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/hhplus/tdd/chap1_2/Chap12ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hhplus.tdd.chap1_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
